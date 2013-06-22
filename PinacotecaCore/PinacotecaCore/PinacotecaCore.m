c0129bd27be4f572747d68eea39d34454d2a85f9
Tobias Kräntzer <info@tobias-kraentzer.de>
2013-06-23 00:48:06 +0200

    Initial Commit

---
+++ PinacotecaCore/PinacotecaCore/PinacotecaCore.m
@@ -0,0 +1,13 @@
+//
+//  PinacotecaCore.m
+//  PinacotecaCore
+//
+//  Created by Tobias Kräntzer on 23.06.13.
+//  Copyright (c) 2013 Tobias Kräntzer. All rights reserved.
+//
+
+#import "PinacotecaCore.h"
+
+@implementation PinacotecaCore
+
+@end
